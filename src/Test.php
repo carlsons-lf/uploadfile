44cad4ed458f0c9ad8e8f2a4d8d439c6c959427d
carlsons <carlsons>
2022-06-01 11:15:39 +0800

    init

---
+++ src/Test.php
@@ -0,0 +1,10 @@
+<?php
+namespace CarlsonsLf\Uploadfile;
+
+class Test
+{
+	
+	public function qq(){
+		return "999999999";
+	}
+}
---
+++ test/qq.php
@@ -0,0 +1,8 @@
+<?php
+require_once '../vendor/autoload.php';
+
+use CarlsonsLf\Uploadfile\Test;
+
+$rr = new Test();
+
+var_dump($rr->qq());
